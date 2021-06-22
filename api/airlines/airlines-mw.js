5159340779f25dfb8b818de96d3cc1efe015382f
Dave Kidd <david.kidd@lambdaschool.com>
2021-06-22 13:05:55 -0600

    final

--- api/airlines/airlines-mw.js
+++ api/airlines/airlines-mw.js
@@ -2,7 +2,7 @@ const checkDest = (req,res,next)=>{
     const {dest} = req.params
     const destUp = dest.toUpperCase()
     if(destUp != "LAX" && destUp != "SFO" && destUp != "CLE"){
-        res.status(404).send("The only destinations American Airlines serves are: SFO, LAX, and CLE\n\nPlease try again in this format: https://muleportnode.herokuapp.com/aa/LAX or SFO or CLE 🙂")
+        res.status(404).send("The only destinations American Airlines serves are: SFO, LAX, and CLE\n\n<br><br>Please try again in this format: https://muleportnode.herokuapp.com/aa/LAX or SFO or CLE 🙂")
     }else{
         next()
     }
